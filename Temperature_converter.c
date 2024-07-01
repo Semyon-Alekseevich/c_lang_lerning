acd1de3a1d0396ca25ee1f7f2894fdea8ca4a39b
Semyon Alekseevich <samjukamail@gmail.com>
2024-07-01 23:42:42 +0500

    I was writing code converter

--- Temperature_converter.c
+++ Temperature_converter.c
@@ -2,6 +2,23 @@
 
 /* печать таблицы соотношения темератур в шкале по Фаренгейту и по Цельсию*/
 
-main () {
+main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;   /*нижняя граница температур*/
+    upper = 300; /*верхняя граница температур*/
+    step = 20;   /*шаг*/
+
+    fahr = lower;
+
+    while (fahr <= upper)
+    {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
     return 0;
 }
